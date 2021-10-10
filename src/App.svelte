7f92742d53f0ec61c858284a703d399f072f4464
sammy-code98 <sammysmart98@yahoo.com>
2021-10-10 01:06:05 +0100

    style hamburger

--- src/App.svelte
+++ src/App.svelte
@@ -12,17 +12,17 @@
     { label: "Item6", href: "#" },
     { label: "Item7", href: "#" },
   ];
-    // mobile icon and display menu
-	let showMobileMenu = false;
+  // mobile icon and display menu
+  let showMobileMenu = false;
 </script>
 
 <div>
   <nav>
     <div class="inner">
-      <div class={`mobile-icon${showMobileMenu ? ' active' : ''}`}>
-        <div class="middle-line" />
+      <div class={`mobile-icon${showMobileMenu ? " active" : ""}`}>
+        <div class="center-line" />
       </div>
-      <ul  class={`navbar-list${showMobileMenu ? ' mobile' : ''}`}>
+      <ul class={`navbar-list${showMobileMenu ? " mobile" : ""}`}>
         {#each navItem as item}
           <li><a href={item.href}>{item.label}</a></li>
         {/each}
@@ -55,7 +55,49 @@
 
     display: flex;
     align-items: center;
-	height: 100%;
+    height: 100%;
+  }
+  /* hamburger icon sytle */
+  .mobile-icon {
+    width: 25px;
+    height: 14px;
+    position: relative;
+    cursor: pointer;
+  }
+  .mobile-icon:after,
+  .mobile-icon:before,
+  .center-line {
+	  content: "";
+	  position: absolute;
+	  width: 100%;
+	  height: 2px;
+	  background-color: #fff;
+	  transition: all 0.4s;
+	  transform-origin: center;
+  }
+  .mobile-icon:before,
+  .center-line{
+	  top: 0;
+  }
+  .mobile-icon:after,
+  .center-line{
+	  bottom: 0;
+  }
+  .mobile-icon:before{
+	  width: 66%;
+  }
+  .mobile-icon:after{
+	  width: 33%;
+  }
+  .center-line{
+	  margin: auto;
+  }
+  .mobile-icon:hover:before,
+  .mobile-icon:hover:after,
+  .mobile-icon.active:before,
+  .mobile-icon.active:after,
+  .mobile-icon.active .center-line {
+    width: 100%;
   }
   main {
     text-align: center;
@@ -63,6 +105,14 @@
     max-width: 240px;
     margin: 0 auto;
     font-family: "Leckerli One", cursive;
+  }
+  .mobile-icon.active:before,
+  .mobile-icon.active:after {
+    top: 50%;
+    transform: rotate(-45deg);
+  }
+  .mobile-icon.active .center-line {
+    transform: rotate(45deg);
   }
 
   h1 {
